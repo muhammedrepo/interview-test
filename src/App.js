2a6b4d09afde4d1a7fe9fe9d7e82c99e54282938
Muhammed Shittu <coolzyte@gmail.com>
2022-08-22 19:02:08 +0100

    minor fix

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
 
   const search = (data) => {
     return data.filter((item) =>
-      keys.some((key) => item[key].toLowerCase().includes(query))
+      keys.some((key) => item[key].toLowerCase().includes(query.toLowerCase()))
     );
   };
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -31,7 +31,6 @@ const SearchBar = ({ setQuery }) => {
           type="search"
           className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-neutral-500 focus:border-neutral-500"
           placeholder="Search Name, Occupation..."
-          required=""
           onChange={(e) => setQuery(e.target.value)}
         />
       </div>
